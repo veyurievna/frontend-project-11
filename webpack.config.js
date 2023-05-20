aabad0f19e0d1501b894fd9a90c2aa41707f0c26
Ekaterina <super.vasilek73@gmail.com>
2023-05-20 20:06:21 +0400

    modified

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,17 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-export default {
+module.exports = {
+  entry: './src/index.js',
   mode: process.env.NODE_ENV || 'development',
   module: {
     rules: [
-      { exclude: /node_modules/ },
       {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+        test: /\.css$/, use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
       },
       {
         test: /\.scss$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader', 'postcss-loader'],
       },
       {
         test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
@@ -24,12 +23,11 @@ export default {
       },
     ],
   },
-
   plugins: [
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
-    new MiniCssExtractPlugin(),
   ],
   output: {
     clean: true,
